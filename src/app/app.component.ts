2ceec1b7593236ebfbc021a8f4e9a8da0656ce99
angel <angel@alphacode.com.br-at-503688510464>
2020-06-25 00:12:54 -0300

    plugin FCM notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import * as firebase from "nativescript-plugin-firebase";
+import {RequestAPI} from '~/app/services/requestAPI';
 
 @Component({
     selector: "ns-app",
@@ -6,11 +8,26 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AppComponent implements OnInit {
 
-    constructor() {
+    constructor(
+        private http: RequestAPI
+    ) {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
+        const platf = 'ios';
         // Init your component properties here.
+        firebase.init({
+            showNotifications: true,
+            showNotificationsWhenInForeground: true,
+            onPushTokenReceivedCallback: (token) => {
+                console.log('[Firebase] onPushTokenReceivedCallback:', { token });
+            },
+            onMessageReceivedCallback: (message: firebase.Message) => {
+                console.log('[Firebase] onMessageReceivedCallback:', { message });
+            }
+        }).then(() => {
+            console.log('[Firebase] Initialized');
+        });
     }
 }
